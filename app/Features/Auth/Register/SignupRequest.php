7d71c97ccea1bdc4bc7478b977a3156ea2e065a8
Sam <samuelcourchesne2004@gmail.com>
2026-04-11 18:30:35 -0400

    feat: add signup request

--- app/Features/Auth/Register/SignupRequest.php
+++ app/Features/Auth/Register/SignupRequest.php
@@ -12,10 +12,24 @@ class SignupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6',
-            'fistName' => 'required|string',
-            'lastName' => 'required|string'
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:6'],
+            'confirm_password' => ['required', 'same:password'],
+
+            'firstName' => ['required', 'string'],
+            'lastName' => ['required', 'string'],
+
+            'encrypted_master_key' => ['required', 'array'],
+            'encrypted_master_key.ciphertext' => ['required', 'string'],
+            'encrypted_master_key.iv' => ['required', 'string'],
+
+            'kdf_salt' => ['required', 'string'],
+
+            'kdf_params' => ['required', 'array'],
+            'kdf_params.algorithm' => ['required', 'string'],
+            'kdf_params.opsLimit' => ['required', 'integer'],
+            'kdf_params.memoryKb' => ['required', 'integer'],
+            'kdf_params.type' => ['required', 'string'],
         ];
     }
 
