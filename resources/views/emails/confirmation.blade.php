94fac98a5bf5fa428a501a890934e2cbd3bb949b
Sam <samuelcourchesne2004@gmail.com>
2026-04-15 10:35:19 -0400

    refac: change email ui

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -1,11 +1,21 @@
 <x-mail::message>
+<p style="text-align: center;">
+    <img
+        src="{{ $message->embed(public_path('images/appIcon.png')) }}"
+        alt="VaultGuardian Logo"
+        width="56"
+        height="56"
+        style="border-radius: 12px;"
+    >
+</p>
+
 # Confirm Your Email
 
 Hi,
 
 Thanks for starting your VaultGuardian account. Confirm your email address to continue your account setup.
 
-<x-mail::button :url="$confirmationUrl">
+<x-mail::button :url="$confirmationUrl" color="blue">
 Verify My Email
 </x-mail::button>
 
