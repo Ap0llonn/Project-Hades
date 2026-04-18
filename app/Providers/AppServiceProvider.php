82a246f45896994cc688afcfc765b8aaba93a345
Sam <samuelcourchesne2004@gmail.com>
2026-04-18 11:10:42 -0400

    fix: return error message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Validation\ValidationException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,9 +24,13 @@ class AppServiceProvider extends ServiceProvider
     {
         RateLimiter::for('login', function (Request $request) {
             $email = strtolower(trim($request->input('email')));
-            return Limit::perMinute(5)->by(
-                $request->input($email) . '|' . $request->ip()
-            );
+            return Limit::perMinute(5)
+                ->by($email . '|' . $request->ip())
+                ->response(function () {
+                    throw ValidationException::withMessages([
+                        'email' => ['Too many login attempts. Please try again in a minute.'],
+                    ]);
+                });
         });
     }
 }
